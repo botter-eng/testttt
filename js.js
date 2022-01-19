51d0ee5ce1dbf432d949db9925e9b290454ce9c1
botter-eng <83904566+botter-eng@users.noreply.github.com>
2022-01-19 20:14:37 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,21 @@
+let textType = document.querySelector('.typeing')
+let textArray = textType.dataset.typeingtext.split("")
+let typeCount = 0;
+
+let autoTypeText = () => {
+    if(typeCount < textType.dataset.typeingtext.length){
+        textType.innerHTML += textType.dataset.typeingtext.charAt(typeCount)
+        typeCount++
+        textArray = textType.dataset.typeingtext.split("")
+    }else {
+        textArray.pop()
+        textType.innerHTML = textArray.join("")
+        if(textArray.length == 0){
+            typeCount = 0
+        }
+    }  
+}
+setInterval(() => {
+    autoTypeText()
+},150)
+
